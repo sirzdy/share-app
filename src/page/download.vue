5541f79252dbf4d27cea8006d6ce5812ea0110e3
zhangdanyang5 <zhangdanyang5@jd.com>
2018-10-12 13:29:35 +0800

    更新下载页面，修复刷新无效的bug

--- src/page/download.vue
+++ src/page/download.vue
@@ -1,19 +1,22 @@
 <template>
-    <div>
-        <iframe width="100%" height="100%" class="iframe" ref="iframe" frameborder="0"></iframe>
-    </div>
+    <section class="container">
+        <div class="refresh" @click="refresh">刷新</div>
+        <iframe width="100%" height="100%" class="iframe" name="iframe" frameborder="0" :src="src"></iframe>
+    </section>
 </template>
 <script>
 import axios from "axios";
 export default {
     data() {
-        return {};
+        return {
+            src: ""
+        };
     },
     created() {
         axios
             .get("/getDownloadPort", {})
             .then(response => {
-                this.$refs.iframe.src =
+                this.src =
                     window.location.protocol +
                     "//" +
                     window.location.hostname +
@@ -23,15 +26,34 @@ export default {
             .catch(function(error) {
                 alert(error);
             });
+    },
+    methods: {
+        refresh() {
+            this.src = this.src + "?" + Date.now();
+        }
     }
 };
 </script>
 <style scoped>
-.iframe {
+.container {
     position: fixed;
     left: 0;
     top: 0;
     width: 100%;
     height: 100%;
+    display: flex;
+    flex-direction: column;
+    background: #eee;
+}
+.refresh {
+    margin: 0 auto;
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    background: #1296db;
+    text-align: center;
+    font-size: 28px;
+    color: #ffffff;
+    cursor: pointer;
 }
 </style>
